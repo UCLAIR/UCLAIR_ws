a9460d33acaf830af01afcc59b06399c80191e62
Albert <morenogonzalezalbert@gmail.com>
2023-01-19 18:41:33 +0000

    change sub node

--- src/localisation/scripts/printer.py
+++ src/localisation/scripts/printer.py
@@ -6,12 +6,12 @@ import cv2
 from cv_bridge import CvBridge
 
 
-
 def callback_frame(data):
     global frame
     frame = bridge.imgmsg_to_cv2(data,"bgr8")
     cv2.imshow('frame',frame)
-    cv2.waitKey(1)  # 1 millisecond
+    cv2.waitKey(17)  # 1 millisecond
+
 
 if __name__ == "__main__":
     rospy.init_node('printer')
