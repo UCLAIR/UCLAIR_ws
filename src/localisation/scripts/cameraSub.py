53e1ca6e3fca1e00a9a4d89f545bb9a0e86422f5
Albert <morenogonzalezalbert@gmail.com>
2023-01-31 15:24:15 +0000

    added strtategy

--- src/localisation/scripts/cameraSub.py
+++ src/localisation/scripts/cameraSub.py
@@ -9,12 +9,14 @@ from cv_bridge import CvBridge
 def callback_frame(data):
     global frame
     frame = bridge.imgmsg_to_cv2(data,"bgr8")
-    cv2.imshow('frame',frame)
+    # cv2.imshow('frame',frame)
     cv2.waitKey(17)  # 1 millisecond
+    rospy.sleep(1) # wait for 1 second
+    return frame
 
 
 if __name__ == "__main__":
-    rospy.init_node('printer')
+    rospy.init_node('cameraSub')
     bridge = CvBridge()
     rospy.Subscriber("/camera", Image, callback_frame)
     rospy.spin()
--- src/localisation/scripts/strategy1.py
+++ src/localisation/scripts/strategy1.py
@@ -1,19 +1,40 @@
+#!/usr/bin/env python3
+#import getBottlesData
 
-import getBottlesData
+import rospy
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge
+global frame
+
+def getCamera(data):
+    global frame
+    frame = bridge.imgmsg_to_cv2(data,"bgr8")
+    cv2.waitKey(17)  # 1 millisecond
+    #rospy.sleep(1) # wait for 1 second
+    
 
 def strategy1():
 
     # INPUT BOTTLES DATA.
-    letter_list,color_list,shape_list = getBottlesData()
+    #letter_list,color_list,shape_list = getBottlesData()
+    #print (letter_list)
 
     # GET FRAME FROM CAMERA
     
+    cv2.imshow('frame',frame)
+    
+    # Wait for a key press
+    while(True):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     # GET CORD OF DRONE WHEN IMAGE WAS TAKEN
 
 
     # INPUT: FRAME 
     # OUTPUT: BOOL (DETECTED TRUE OR FALSE)
+
 
 
     # IF DETECTED
@@ -31,5 +52,13 @@ def strategy1():
     pass
 
 if __name__ == '__main__':
-    strategy1()
+
+    rospy.init_node('cameraSub')
+    bridge = CvBridge()
+    rospy.Subscriber("/camera", Image, getCamera)
+    rospy.spin()
     
+    strategy1()
+    cv2.destroyAllWindows()
+
+
