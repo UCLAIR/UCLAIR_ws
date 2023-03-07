307f088a328a1df9e812e39a95408595017db305
jackhau0212 <jackhau0212@gmail.com>
2023-03-07 15:29:08 +0000

    added changes

--- src/uav_navigation/scripts/testing.py
+++ src/uav_navigation/scripts/testing.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
 
 import rospy
-from mavros_msgs.msg import TerrainReport
+from navigation_functions import *
+from std_msgs.msg import Float64MultiArray, Bool
 
-def callback(msg):
-    rospy.loginfo(msg.current_height)
-
-def listener():
-    rospy.init_node("testing")
-
-    rospy.Subscriber(
-        name="mavros/terrain/report",
-        data_class=TerrainReport,
-        callback=callback
-    )
-
-    rospy.spin()
 
 if __name__ == "__main__":
-    listener()
+
+    try:
+        rospy.init_node("uav_execute_node")
+
+        uav = Navigation()
+
+        uav.wait4connect()
+
+        uav.set_mode("GUIDED")
+
+        uav.wait4connect()
+
+        uav.takeoff(10)
+
+        rate = rospy.Rate(10)
+
+        while not rospy.is_shutdown():
+            rate.sleep()
+
+            
 
 
 
