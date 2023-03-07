bddd371130a26eb089851dfef94dd60dd683bc73
Albert <68657879+xmoree@users.noreply.github.com>
2023-03-07 19:36:02 +0000

    Update colour recognition.py

--- src/yolov8_ros/colour recognition.py
+++ src/yolov8_ros/colour recognition.py
@@ -3,6 +3,7 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+import webcolors
 
 def rgb_to_hex(rgb_color):
     hex_color = '#'
@@ -29,10 +30,33 @@ counts = Counter(color_labels)
 ordered_colors = [center_colors[i] for i in counts.keys()]
 hex_colors = [rgb_to_hex(ordered_colors[i]) for i in counts.keys()]
 
-shape_colour = hex_colors[0]
-text_colour = hex_colors[1]
+# Define reference colors
+color_names = {
+    (255, 255, 255): 'white',
+    (0, 0, 0): 'black',
+    (128, 128, 128): 'gray',
+    (255, 0, 0): 'red',
+    (0, 0, 255): 'blue',
+    (0, 255, 0): 'green',
+    (255, 255, 0): 'yellow',
+    (128, 0, 128): 'purple',
+    (165, 42, 42): 'brown',
+    (255, 165, 0): 'orange'
+}
 
+# Get the closest reference color and its name for each color in the image
+color_names = []
+for hex_color in hex_colors:
+    rgb_color = webcolors.hex_to_rgb(hex_color)
+    closest_color = None
+    closest_distance = float('inf')
+    for ref_color, name in color_names.items():
+        distance = np.linalg.norm(np.array(rgb_color) - np.array(ref_color))
+        if distance < closest_distance:
+            closest_distance = distance
+            closest_color = name
+    color_names.append(closest_color)
 
-
-
-
+# Print the detected colors and their corresponding reference colors
+for i in range(len(color_names)):
+    print("Detected color: " + hex_colors[i] + ", Reference color: " + color_names[i])
