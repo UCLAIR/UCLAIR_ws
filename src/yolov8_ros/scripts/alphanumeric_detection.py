ababbc9eb90e7e5f35938ab9224922b2b5a36b2b
Albert <68657879+xmoree@users.noreply.github.com>
2023-03-08 17:57:31 +0000

    Update alphanumeric_detection.py

--- src/yolov8_ros/scripts/alphanumeric_detection.py
+++ src/yolov8_ros/scripts/alphanumeric_detection.py
@@ -1,11 +1,11 @@
 import cv2
 import pytesseract
 
-def alphanumeric_detection():
+def alphanumeric_detection(image):
   pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
   # Grayscale, Gaussian blur, Otsu's threshold
-  image = cv2.imread('1.png')
+  # image = cv2.imread('1.png')
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
   blur = cv2.GaussianBlur(gray, (3,3), 0)
   thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
