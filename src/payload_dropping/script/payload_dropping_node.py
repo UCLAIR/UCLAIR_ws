9a5904843f67439c0ad1d243fe98e3ea8adb1da2
jackhau0212 <jackhau0212@gmail.com>
2023-03-30 12:03:01 +0100

    added payload_dropping_node

--- src/payload_dropping/script/payload_dropping_node.py
+++ src/payload_dropping/script/payload_dropping_node.py
@@ -26,6 +26,32 @@ class PayloadDropping:
 
     def drop_complete_sub_cb(self, msg):
         self.drop_complete = msg.data
+
+
+if __name__ == "__main__":
+    try:
+        rospy.init_node("payload_dropping_node")
+        
+        counter = 0
+
+        payload_dropping = PayloadDropping()
+
+        while not rospy.is_shutdown():
+
+            while counter < 1 and payload_dropping.execute_drop:
+                rospy.loginfo("Dropping bottle")
+
+                payload_dropping.publish(True)
+                
+                counter += 1
+
+            if payload_dropping.execute_drop == False:
+                counter = 0
+
+    except rospy.ROSInterruptException:
+        pass
+
+
         
 
 
