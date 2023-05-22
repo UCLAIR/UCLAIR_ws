cefb775dc60b822e5c969297a91e191493464f79
jackh <jackhau0212@gmail.com>
2023-05-22 17:44:31 +0100

    changes in testing folder

--- src/uav_navigation/scripts/testing/path_planning_test.py
+++ src/uav_navigation/scripts/testing/path_planning_test.py
@@ -1,7 +1,8 @@
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 import math
+
 
 # Constants
 K_ATTRACT = 0.1  # Attractive force coefficient
@@ -19,10 +20,14 @@ def get_attractive_force(start, position, goal):
     distance_star = np.linalg.norm(goal - start) * 0.5
 
     if distance_goal <= distance_star:
-        return K_ATTRACT * (goal - position)
 
+        return K_ATTRACT * (goal - position)
+    
     else:
         return distance_star * K_ATTRACT * (goal - position) / distance_goal
+    
+    # return K_ATTRACT * (goal - position)
+
 
 
 def get_repulsive_force(position, goal, obstacle, radius):
@@ -36,15 +41,16 @@ def get_repulsive_force(position, goal, obstacle, radius):
     #     repulsive = K_REPULSE/distance_obstacle**2 * (1/distance_obstacle - 1/radius) * (1 - math.exp(-((goal[0] - position[0])**2 + (goal[1] - position[1])**2)/drone_radius**2))
 
     #     return repulsive
-
+    
     # else:
     #     return np.zeros_like(position)
 
-    # Krogh's potential function
+
+    #Krogh's potential function
     distance_obstacle = np.linalg.norm(obstacle - position)
 
     if distance_obstacle <= radius:
-        repulsive = K_REPULSE / (distance_obstacle ** 2) * ((1.0 / distance_obstacle) - (1.0 / radius)) * (position - obstacle)
+        repulsive = K_REPULSE/(distance_obstacle**2) * ((1.0 / distance_obstacle) - (1.0 / radius)) * (position - obstacle)
 
         return repulsive
     else:
@@ -66,7 +72,7 @@ def get_next_waypoint(start, position, goal, obstacles, radius):
     # Repulsive forces from current position to obstacles
     repulsive_forces = [get_repulsive_force(position, goal, obs, radius) for obs in obstacles]
 
-    # print(f"Attractive Force: {attractive_force}, Repulsive Forces: {repulsive_forces}")
+    #print(f"Attractive Force: {attractive_force}, Repulsive Forces: {repulsive_forces}")
 
     # Calculate the net force
     net_force = attractive_force
@@ -77,7 +83,6 @@ def get_next_waypoint(start, position, goal, obstacles, radius):
     next_position = position + net_force
 
     return next_position
-
 
 def get_path(start, goal, obstacles, radius, num_waypoints):
     waypoints = [start]
@@ -98,13 +103,12 @@ def get_path(start, goal, obstacles, radius, num_waypoints):
         # Creating virtual obstacle if stuck at local minima
         if len(waypoints) >= 2:
             if (abs(waypoints[-1][0] - waypoints[-2][0]) / waypoints[-1][0]) < 0.0000001 \
-                    and (abs(waypoints[-1][1] - waypoints[-2][1]) / waypoints[-1][1]) < 0.0000001:
-                obstacles_distance = [((obs[0] - waypoints[-1][0]) ** 2 + (obs[1] - waypoints[-1][1]) ** 2) for obs in
-                                      obstacles]
+                and (abs(waypoints[-1][1] - waypoints[-2][1])/waypoints[-1][1]) < 0.0000001:
+
+                obstacles_distance = [((obs[0] - waypoints[-1][0]) ** 2 + (obs[1] - waypoints[-1][1]) ** 2) for obs in obstacles]
 
                 closest_obstacle_index = obstacles_distance.index(min(obstacles_distance))
-                obstacles.append(
-                    np.array([obstacles[closest_obstacle_index][0] + 0.0001, obstacles[closest_obstacle_index][1]]))
+                obstacles.append(np.array([obstacles[closest_obstacle_index][0] + 0.0001, obstacles[closest_obstacle_index][1]]))
 
         current_position = next_waypoint
 
@@ -120,58 +124,50 @@ def get_path(start, goal, obstacles, radius, num_waypoints):
 
     minimum_waypoints.append(goal.tolist())
     minimum_waypoints.insert(0, start.tolist())
-    
+
     return minimum_waypoints
 
 
 # Example usage
 start = np.array([0, 0])
-goal = np.array([100, 100])
-obstacles = [np.array([40, 50]), np.array([50, 40])]
+goal = np.array([100, 125])
+obstacles = [np.array([35, 35])]
 radius = 200
 num_waypoints = 1000
 
 path = get_path(start, goal, obstacles, radius, num_waypoints)
+print(path)
+print(len(path))
+datax = [p[0] for p in path]
+datay = [p[1] for p in path]
 
-plotx = [p[0] for p in path]
-ploty = [p[1] for p in path]
+# create the figure and axes object
+fig, ax = plt.subplots()
 
-x = []
-y = []
-
-figure, ax = plt.subplots()
-figure.set_figwidth(10)
-figure.set_figheight(10)
-
-ax.plot(start[0], start[1], "r+")
 ax.plot([obs[0] for obs in obstacles], [obs[1] for obs in obstacles], 'ko')
 ax.plot([goal[0]], [goal[1]], 'ro')
-circle = plt.Circle((start[0], start[1]), 100, fill = False, color="pink")
 
-ax.set_aspect(1)
-ax.add_artist(circle)
+# create an empty line object
+line, = ax.plot([], [])
 
-# Setting limits for x and y axis
-ax.set_xlim(0, 150)
-ax.set_ylim(0, 150)
-
-# Since plotting a single graph
-line, = ax.plot(0, 0)
-
-
-def animation_function(i):
-    x.append(plotx[i])
-    y.append(ploty[i])
-
-    line.set_xdata(x)
-    line.set_ydata(y)
+# define the animation function
+num_frames = len(datax)
+frame_interval = 100
+def animate(frame):
+    # get the x and y coordinates of the line for the current frame
+    x = datax[:frame+1]
+    y = datay[:frame+1]
+    # update the data of the line object
+    line.set_data(x, y)
+    # return the updated line
     return line,
 
+# create the animation object
+animation = FuncAnimation(fig, animate, frames=num_frames, interval=frame_interval)
 
-animation = FuncAnimation(figure, func=animation_function, frames = np.arange(0, len(ploty)), interval=100, repeat=False)
+# set the x and y limits of the axes
+ax.set_xlim([-100, 300])
+ax.set_ylim([-100, 300])
 
-plt.legend(["UAV Current Position", "obstacles", "Goal", "LiDAR Range (m)", "Path with Local Minima Solution"])
-plt.ylabel("y (m)")
-plt.xlabel("x (m)")
-plt.title("Local Minima Solution")
+# show the animation
 plt.show()
--- src/uav_navigation/scripts/testing/testing3.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-import rospy
-from navigation_functions import *
-
