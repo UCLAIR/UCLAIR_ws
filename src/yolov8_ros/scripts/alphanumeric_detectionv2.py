62c51c7222a052041b6154db81a13f300146a6f4
Milan-Kh <zcemmkk@ucl.ac.uk>
2023-05-24 17:11:23 +0100

    more efficient v1

--- src/yolov8_ros/scripts/alphanumeric_detectionv2.py
+++ src/yolov8_ros/scripts/alphanumeric_detectionv2.py
@@ -15,7 +15,7 @@ class OCRDetectionClassifier:
 
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        _, mask = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)
+        _, mask = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
 
         return mask
 
