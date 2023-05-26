add7d28900a7c0270b0cf9403df735f51a16e7d0
uclair2023 <uclairobotics@gmail.com>
2023-05-26 15:17:29 +0200

    changes database.py (maybe be wrong)

--- src/yolov8_ros/scripts/database.py
+++ src/yolov8_ros/scripts/database.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import rospy
+from std_msgs.msg import Float64MultiArray
 from detection_msgs.msg import BoundingBox, BoundingBoxes
 import pandas as pd
 import math
@@ -16,9 +17,39 @@ class Dataf:
         self.dataf=pd.DataFrame(columns=self.column_names)
         self.rowdf=pd.DataFrame(columns=self.column_names)
         
-        self.bottles_df = pd.read_csv(f'/home/{getuser()}/UCLAIR_ws/src/yolov8_ros/database/BOTTLESDATA.txt', sep=',', header=None, names=['Class', 'character', 'color_shape', 'color_char'])
+        self.bottles_df = pd.read_csv(f'/home/{getuser()}/UCLAIR_ws/src/yolov8_ros/database/BOTTLESDATA.txt', sep=',', header=None, names=['color_shape', 'color_char', 'Class', 'character'])
         self.matches_df=pd.DataFrame(columns=self.column_names)
         self.matching_rows=pd.DataFrame(columns=self.column_names)
+        
+        self.GPS_drop_1_pub = rospy.Publisher(
+            name="GPS_drop_1",
+            data_class=Float64MultiArray,
+            queue_size=10
+        )
+        
+        self.GPS_drop_2_pub = rospy.Publisher(
+            name="GPS_drop_2",
+            data_class=Float64MultiArray,
+            queue_size=10
+        )
+
+        self.GPS_drop_3_pub = rospy.Publisher(
+            name="GPS_drop_3",
+            data_class=Float64MultiArray,
+            queue_size=10
+        )
+
+        self.GPS_drop_4_pub = rospy.Publisher(
+            name="GPS_drop_4",
+            data_class=Float64MultiArray,
+            queue_size=10
+        )
+
+        self.GPS_drop_5_pub = rospy.Publisher(
+            name="GPS_drop_5",
+            data_class=Float64MultiArray,
+            queue_size=10
+        )
 
         self.current_altitude_sub = rospy.Subscriber(
             name="BoundingBoxes",
@@ -26,10 +57,37 @@ class Dataf:
             callback=self.data_sub_cb
         )
 
+        # Convert the data into publisherable data for Float64MultiArray()
+    def publish_float64multiarray_data(self, d):
+        publishing_data = Float64MultiArray()
+
+        publishing_data.data = d
+
+        return publishing_data
+
+    def publish_data(self, i, lat, lon):
+        pub_GPS = self.publish_float64multiarray_data([lat, lon])
+
+
+        if i == 0:
+            self.GPS_drop_1_pub(pub_GPS)
+
+        elif i == 1:
+            self.GPS_drop_2_pub(pub_GPS)
+
+        elif i == 2:
+            self.GPS_drop_3_pub(pub_GPS)
+
+        elif i == 3:
+            self.GPS_drop_4_pub(pub_GPS)
+        
+        elif i == 4:
+            self.GPS_drop_5_pub(pub_GPS)
+
     def data_sub_cb(self, msg):
         for bb in msg.bounding_boxes:
             dist=math.sqrt(pow(bb.xDISTANCE,2)+pow(bb.yDISTANCE,2))
-            row_dict = {"Class":bb.Class,"probability":bb.probability,"long":bb.long,"lat":bb.lat,"Distance":dist,"character":bb.character,"color_shape":bb.color_shape,"color_char":bb.color_char}
+            row_dict = {"Class":bb.Class,"probability":bb.probability,"long":bb.long,"lat":bb.lat,"Distance":bb.xDISTANCE,"character":bb.character,"color_shape":bb.color_shape,"color_char":bb.color_char}
             # Check if row with same combination of class, character, color_shape, and color_char exists in the dataframe
             if not any(value == "null" for value in [bb.character, bb.color_shape, bb.color_char]):
                 if self.dataf.empty:
@@ -64,6 +122,27 @@ if __name__ == "__main__":
             (RESULTS.dataf['character'] == row['character']) &
             (RESULTS.dataf['color_shape'] == row['color_shape']) &
             (RESULTS.dataf['color_char'] == row['color_char']))]
+
+            if RESULTS.matches_df.empty:
+                RESULTS.matches_df = RESULTS.dataf.loc[
+                (RESULTS.dataf['Class'] == row['Class']) &
+                (RESULTS.dataf['color_shape'] == row['color_shape'])
+                ]
+
+                if RESULTS.matches_df.empty:
+                    RESULTS.publish_data(i, 0, 0)
+                else:
+                    lon = float(RESULTS.matches_df["long"][0])
+                    lat = float(RESULTS.matches_df["lat"][0])
+
+                    RESULTS.publish_data(i, lat, lon)
+
+
+            else:
+                lon = float(RESULTS.matches_df["long"])
+                lat = float(RESULTS.matches_df["lat"])
+
+                RESULTS.publish_data(i, lat, lon)
 
             # If there are no matches, continue to the next row
             if not RESULTS.matches_df.empty:
