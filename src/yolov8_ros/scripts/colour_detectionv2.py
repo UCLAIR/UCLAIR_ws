b7a1a53f228c59f16298ac173195c71f6131c4f7
Milan-Kh <zcemmkk@ucl.ac.uk>
2023-05-31 14:53:28 +0100

    minor change

--- src/yolov8_ros/scripts/colour_detectionv2.py
+++ src/yolov8_ros/scripts/colour_detectionv2.py
@@ -11,7 +11,7 @@ class ImageProcessingClassifier:
         "GRAY": (169, 169, 169),
         "RED": (255, 0, 0),
         "BLUE": (0, 0, 255),
-        "GREEN": (0, 170, 20),
+        "GREEN": (55, 170, 70),
         "YELLOW": (255, 255, 0),
         "PURPLE": (128, 0, 128),
         "BROWN": (92, 64, 51),
