6b94d443b3ebbd266c5b9a27c76346d7370d9b25
jackh <jackhau0212@gmail.com>
2023-06-08 14:45:25 +0100

    potential fixed NN BIG DATA colour classification

--- src/yolov8_ros/scripts/colour_detectionv2.py
+++ src/yolov8_ros/scripts/colour_detectionv2.py
@@ -2,9 +2,14 @@ import sys
 import numpy as np
 from sklearn.cluster import KMeans
 import cv2
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
 
 class ImageProcessingClassifier:
+    model = tf.keras.models.load_model("colormodel_trained_90.h5")
+    
     COLORS = {
         "WHITE": (255, 255, 255),
         "BLACK": (0, 0, 0),
@@ -17,6 +22,20 @@ class ImageProcessingClassifier:
         "BROWN": (92, 64, 51),
         "ORANGE": (255, 165, 0),
         "PINK": (255, 145, 175)
+    }
+    
+    color_dict={
+        0 : 'Red',
+        1 : 'Green',
+        2 : 'Blue',
+        3 : 'Yellow',
+        4 : 'Orange',
+        5 : 'Pink',
+        6 : 'Purple',
+        7 : 'Brown',
+        8 : 'Grey',
+        9 : 'Black',
+        10 : 'White'
     }
 
     def __init__(self, image_path):
@@ -55,38 +74,56 @@ class ImageProcessingClassifier:
         kmeans = KMeans(n_clusters=k)
         kmeans.fit(image_flat)
         colours = kmeans.cluster_centers_
+        
+        output_colours = []
+        
+        for i in range(len(colours)):
+            output_colours.append(self.predict_color(colours[i][0], colours[i][1], colours[i][2]), (kmeans.labels_).count(i))
+            
+        return output_colours.sort(key = lambda row: row[1])
 
-        colour_names = []
-        for colour in colours:
-            closest_name = ""
-            closest_distance = sys.float_info.max
-            for name, rgb in self.COLORS.items():
-                distance = self.compute_distance(rgb, colour)
-                if distance < closest_distance:
-                    closest_name = name
-                    closest_distance = distance
-            colour_names.append(closest_name)
+        ###############################################################################
+        ##############MILAN############################################################
+        # colour_names = []
+        # for colour in colours:
+        #     closest_name = ""
+        #     closest_distance = sys.float_info.max
+        #     for name, rgb in self.COLORS.items():
+        #         distance = self.compute_distance(rgb, colour)
+        #         if distance < closest_distance:
+        #             closest_name = name
+        #             closest_distance = distance
+        #     colour_names.append(closest_name)
 
-        pixel_counts = []
-        for i in range(k):
-            pixel_counts.append(list(kmeans.labels_).count(i))
+        # pixel_counts = []
+        # for i in range(k):
+        #     pixel_counts.append(list(kmeans.labels_).count(i))
 
-        colour_map = {}
-        for i in range(k):
-            if colour_names[i] != 'PINK':
-                colour_map[colour_names[i]] = pixel_counts[i]
+        # colour_map = {}
+        # for i in range(k):
+        #     if colour_names[i] != 'PINK':
+        #         colour_map[colour_names[i]] = pixel_counts[i]
 
-        sorted_colours = sorted(colour_map.items(), key=lambda x: x[1], reverse=True)
+        # sorted_colours = sorted(colour_map.items(), key=lambda x: x[1], reverse=True)
 
-        dominant_colours = []
-        for colour in sorted_colours[:2]:
-            dominant_colours.append(colour[0])
+        # dominant_colours = []
+        # for colour in sorted_colours[:2]:
+        #     dominant_colours.append(colour[0])
 
-        if len(dominant_colours) > 1:
-            return dominant_colours[0], dominant_colours[1]
-        else:
-            return dominant_colours[0], 'null'
+        # if len(dominant_colours) > 1:
+        #     return dominant_colours[0], dominant_colours[1]
+        # else:
+        #     return dominant_colours[0], 'null'
+        ##############MILAN############################################################
 
+    def predict_color(self, Red, Green, Blue):
+        rgb = np.asarray((Red, Green, Blue)) #rgb tuple to numpy array
+        input_rgb = np.reshape(rgb, (-1,3)) #reshaping as per input to ANN model
+        color_class_confidence = self.model.predict(input_rgb) # Output of layer is in terms of Confidence of the 11 classes
+        color_index = np.argmax(color_class_confidence, axis=1) #finding the color_class index from confidence
+        color = self.color_dict[int(color_index)]
+        return color
+    
     @staticmethod
     def compute_distance(rgb1, rgb2):
         return np.linalg.norm(np.array(rgb1) - np.array(rgb2))
